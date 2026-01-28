73d83efaf8ab2eeae0d04d4ef0fc54fbe8f073d2
skywlkr <sean0109@naver.com>
2026-01-28 17:59:54 +0900

    commit

--- src/main/java/io/github/skywlkr/myrestfulservice/exception/UserNotFoundException.java
+++ src/main/java/io/github/skywlkr/myrestfulservice/exception/UserNotFoundException.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Getter
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.NOT_FOUND)   // 500 error 가 아닌, 404 not found 를 리턴한다.
 public class UserNotFoundException extends RuntimeException {
 
     private final Integer id;
